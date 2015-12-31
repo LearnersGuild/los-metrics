276b23ca6ac242883381263f7c25b6acef9e7a83
jeffreywescott <jeffrey@learnersguild.org>
2015-12-30 16:15:45 -0800

    client is just the app entry point, not a container

--- client/index.jsx
+++ client/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-import Root from '../../common/containers/Root'
+import Root from '../common/containers/Root'
 
 ReactDOM.render(<Root />, document.getElementById('root'))
