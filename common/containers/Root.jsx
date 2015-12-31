b71781a9d13acce103e3f36808b1b7bf6466d53d
jeffreywescott <jeffrey@learnersguild.org>
2015-12-31 10:08:31 -0800

    use Bootstrap mixins

--- common/containers/Root.jsx
+++ common/containers/Root.jsx
@@ -7,13 +7,13 @@ import styles from './Root.scss'
 export class Root extends React.Component {
   render() {
     return (
-      <div className={`${styles.root} container-fluid`}>
+      <div className={styles.root}>
         <div className="row">
-          <section className="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+          <section className={styles.content}>
             <div className="display-3">Product Metrics</div>
             <div>
-              <a className="btn btn-primary" href="/docs/#!/default">View API Docs</a>
-              <a className="btn btn-primary" onClick={() => this.props.dispatch(pushPath('/example'))}>Example</a>
+              <a className={styles.btn} href="/docs/#!/default">View API Docs</a>
+              <a className={styles.btn} onClick={() => this.props.dispatch(pushPath('/example'))}>Example</a>
             </div>
           </section>
         </div>
