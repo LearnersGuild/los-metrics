52a238403ce5446c27683d68ca900acefd0e9f46
jeffreywescott <jeffrey@learnersguild.org>
2016-02-13 14:43:45 -0800

    add 15 seconds to ensure the job is run immediately

--- server/configureScheduler.js
+++ server/configureScheduler.js
@@ -4,8 +4,10 @@ import computeMetrics from './computeMetrics'
 
 function setupScheduler() {
   // schedule one periodic job, but also run it once (in case data doesn't yet exist)
+  const inABit = new Date(new Date().getTime() + 15000)
+  inABit.set
   schedule.scheduleJob(process.env.COMPUTATION_SCHEDULE, computeMetrics)
-  schedule.scheduleJob(new Date(), computeMetrics)
+  schedule.scheduleJob(inABit, computeMetrics)
 }
 
 setupScheduler()
