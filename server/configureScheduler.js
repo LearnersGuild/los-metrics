7b8b56c6e620f2d0ab0f40508d01561a88426ad4
jeffreywescott <jeffrey@learnersguild.org>
2016-02-15 12:59:04 -0800

    only run immediate job when in production

--- server/configureScheduler.js
+++ server/configureScheduler.js
@@ -5,9 +5,10 @@ import computeMetrics from './computeMetrics'
 function setupScheduler() {
   // schedule one periodic job, but also run it once (in case data doesn't yet exist)
   const inABit = new Date(new Date().getTime() + 15000)
-  inABit.set
   schedule.scheduleJob(process.env.COMPUTATION_SCHEDULE, computeMetrics)
-  schedule.scheduleJob(inABit, computeMetrics)
+  if (process.env.NODE_ENV === 'production') {
+    schedule.scheduleJob(inABit, computeMetrics)
+  }
 }
 
 setupScheduler()
