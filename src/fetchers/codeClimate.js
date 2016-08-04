7397bd12c6b7caa0b6adc0e9921a67b987e6851d
jeffreywescott <jeffrey@learnersguild.org>
2016-08-04 12:19:59 -0700

    test codeClimate fetchers

--- src/fetchers/codeClimate.js
+++ src/fetchers/codeClimate.js
@@ -3,12 +3,11 @@ import config from 'config'
 import apiFetch from '../util/apiFetch'
 
 function apiURL(path) {
-  return `${config.get('api.codeClimate.baseURL')}/${path}?api_token=${config.get('api.codeClimate.token')}`
+  return `${config.get('api.codeClimate.baseURL')}${path}?api_token=${config.get('api.codeClimate.token')}`
 }
 
-export function getRepositoryIds() {
+export function getRepositories() {
   return apiFetch(apiURL('/repos'))
-    .then(repos => repos.map(repo => repo.id))
 }
 
 export function getRepositoryMetrics(repoId) {
