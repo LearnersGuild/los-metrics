456a2ebb01fc38be2c1ae59ba40e2ba9b9c4d253
jeffreywescott <jeffrey@learnersguild.org>
2016-12-12 12:22:26 -0800

    count errors by project and status

--- src/metrics/usability/index.js
+++ src/metrics/usability/index.js
@@ -3,6 +3,7 @@ import logErrorAndExit from '../../util/logErrorAndExit'
 import {getAnalysis} from '../../fetchers/keen'
 
 import {saveIssueMetrics, saveSupportMetrics} from './satisfaction'
+import {saveIssueMetrics as saveErrorMetrics} from './effectiveness'
 
 async function getUsabilityMetrics() {
   const {result: issueCountsByRepoAndLabel} = await getAnalysis('usability', 'maximum', {
@@ -10,6 +11,13 @@ async function getUsabilityMetrics() {
     targetProperty: 'count',
     timeframe: 'this_1_day',
     groupBy: ['repoName', 'label', 'state'],
+  })
+
+  const {result: errorCountsByProjectAndStatus} = await getAnalysis('usability', 'maximum', {
+    eventCollection: 'losErrorCountsByProject',
+    targetProperty: 'count',
+    timeframe: 'this_1_day',
+    groupBy: ['projectSlug', 'status'],
   })
 
   const {result: supportMessageCount} = await getAnalysis('usability', 'maximum', {
@@ -23,7 +31,12 @@ async function getUsabilityMetrics() {
       repoName,
       label,
       state,
-      issueCount: result
+      issueCount: result,
+    })),
+    errorCounts: errorCountsByProjectAndStatus.map(({projectSlug, status, result}) => ({
+      projectSlug,
+      status,
+      errorCount: result,
     })),
     supportMessageCount,
   }
@@ -37,10 +50,15 @@ async function run() {
     // satisfaction: save metrics about users requesting support
     await saveSupportMetrics()
 
+    // effectiveness: save metrics about number of unresolved errors
+    await saveErrorMetrics()
+
     // save and display roll-up metrics
     const usability = await getUsabilityMetrics()
     console.info('Issue Counts by Repository and Label:')
     console.info(table(usability.issueCounts, {includeHeaders: true}))
+    console.info('\nError Counts by Project and Status:')
+    console.info(table(usability.errorCounts, {includeHeaders: true}))
     console.info('\nNumber of #support messages in past day:', usability.supportMessageCount)
   } catch (err) {
     logErrorAndExit(err)
